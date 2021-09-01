f1c690cba93425d9e986f3cae57ef74a019a6f77
arosenmund <aaron.rosenmund@gmail.com>
2021-09-01 08:43:44 -0400

    update for intel

--- LAB_FILES/incident-response-kit/initial-triage/Run-Initial-Triage.ps1
+++ LAB_FILES/incident-response-kit/initial-triage/Run-Initial-Triage.ps1
@@ -10,12 +10,12 @@ $datapath = "$name-$domain"
 new-item -Name $datapath -Path . -ItemType directory
 
 ## Start the timer, and record your actions.
-captains_log($entry, $datapath){
+function captains_log($entry, $datapath){
     $datetime = Get-Date 
     $timezone = (Get-TimeZone).DisplayName
     $stardate = "Captains Log Entry, Startdate: $datetime $timezone \n"
     $stardate| out-file -Append -FilePath ./$datapath/master-station-log.txt
-    $entry | oute-file -Append -FilePath ./$datapath/master-station-log.txt
+    $entry | out-file -Append -FilePath ./$datapath/master-station-log.txt
     write-host "Captains log entry complete." -ForegroundColor Green
 }
 
@@ -24,16 +24,17 @@ captains_log($entry, $datapath){
 get-date >> ./data/master-station-log.txt
 Get-TimeZone >> ./$datapath/master-station-log.txt
 Get-NetIPInterface >> $datapath/master-station-log.txt
-Get-NetIPAddress = >> $datapath/master-station-log.txt
+Get-NetIPAddress >> $datapath/master-station-log.txt
 #records all your activity
 start-transcript -Path ./$datapath/powershelltranscript.txt -Append
+psr
 get-computerinfo
 wmic OS get OSArchitecture
 
 
 
 ## Memory Dump - Do a quick memory pull we will look more at this later.
-winpmem_mini_x64 $datapath/$computername-memorydump.dmp 
+./winpmem_mini_x64_rc2.exe $datapath/$computername-memorydump.dmp 
 captains_log "Took full memory image." $datapath
 
 
@@ -41,7 +42,7 @@ captains_log "Took full memory image." $datapath
 captains_log "Desktop has ransom note file, grabbing properties and hash as potential IOC" $datapath
 $note_file_details = get-itemproperty -Path c:\Users\Administrator\Desktop\theinvincibleironcat.txt; $note_file_details
 $note_file_details | out-file -Filepath $datapath/IOC-ransom-note.txt -append
-$filehash = get-filehash -Path C:\Users\Administrator\Desktop\theinvincibleironcat.txt; $filehash
+$filehash = get-filehash -Algorithm md5 -Path C:\Users\Administrator\Desktop\theinvincibleironcat.txt; $filehash
 $filehash | out-file -Filepath $datapath/IOC-ransom-note.txt -append
 copy-item -Path c:\Users\Administrator\Desktop\theinvincibleironcat.txt  $datapath
 ### Take a guess here to scope and look for this file name/hash throughout the file system.
@@ -54,12 +55,11 @@ captains_log "Running network inspection to look for expected network beaconing 
 $netConn = get-nettcpconnection; $netConn
 netstat -anob
 $netconn | export-clixml -Path $datapath/netconn.xml
-$netconn | out-file -Path $datapath/netconn.txt
+$netconn | out-file -FilePath $datapath/netconn.txt
 ### May or may not catch connection information.
 ## tcpview & portmon
-tcpvcon64.exe -a /accepteula
+./SysInternalsSuite/tcpvcon64.exe -a /accepteula
 
-    ## 
 
 # Identify network activity to resolved hello.iamironcat.com & find process
 
@@ -67,42 +67,29 @@ tcpvcon64.exe -a /accepteula
 $procs = Get-Process; $procs #find process name to use as IOC
 $procs |out-file -path $datapath/processes.txt
 #WMIC get specific process information based on PID found in get-process info.
-wmic query all format html
-procexp64.exe
+wmic process list full /format:htable >> $datapath/process.html
+./SysInternalsSuite/procexp64.exe -accepteula
 #Procexp process explorer to inspect this process and understand its tcp activity!
 
-#tcpview & portmon
-    
+#tcpview
+./SysInternalsSuite/tcpview64.exe
+## browse to domain & ip
+stop-transcript
+
+
 
 #DEMO: Use IOC's to find other devices.
 
 ## Asset list from ARP table, layer 2
 ## Asset list from Active Directory Pull 
-    Get-adcomputers -filter *
+#Get-adcomputers -filter *
+
 foreach ($c in $computers) {
     get-childitem -Path #if note file ending in ENCRYPTED or HASH matches
     Get-Process #if process like ironcatwuzhere
     get-nettcpconnection #if port like 8080 but doesn't come back in line with the other behavior!
 }
 
-
-#Module: Intel
-
-# Demo: Malware Intel
-
-## Strings
-
-## Google Strings
-
-
-## Domain Information - clean domain
-
-## Virus Total - find old sample
-
-## Run on anyruns - not a lot of results
-
-## Atomic indicators
-#hash of exe to be used to find other exe!
 
 
 #Module: Host Collection
